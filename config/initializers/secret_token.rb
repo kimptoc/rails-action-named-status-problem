6bdec88aad7a6358118e3dc5b30929c851efae6b
Chris Kimpton <chris@kimptoc.net>
2011-11-24 14:45:26 +0000

    starter

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails313scratch::Application.config.secret_token = '95c461c9950d803b2c827454ab5d38e8e7bc83bca687e9b93629dc9ab707527f677e19495f6a26291383b38d86d3ed4904e35841d1e797fbb4ede1057b9f3446'
