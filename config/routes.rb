f64c2229bfd845396791f18ea583812c598d59f0
Chris Kimpton <chris@kimptoc.net>
2011-11-24 17:37:34 +0000

    added widgets scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails313scratch::Application.routes.draw do
+  resources :widgets
+
+  match 'widgets/:id/foobar' => 'widgets#foobar'
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
